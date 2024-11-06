b5317cf3616dd9199f0110570af69380a4460970
IE Dev 6 <ie.dev.6@hotmail.com>
2024-11-06 10:40:49 -0700

    routes

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/todo', function () {
+    return 'All Todo';
+});
+
+Route::post('/todo', function () {
+    return 'dsafdgdgaf save data';
+});
+
+
